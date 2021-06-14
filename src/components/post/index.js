df8d221675f22b00d86f67fed9eff53b13f9ee7d
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-14 08:38:46 -0300

    fetch posts

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 function Post({ bgColor, title, text }) {
   return (
     <article
-      className='wrapper'
+      className="wrapper"
       style={{
         backgroundColor: bgColor,
       }}
     >
-      <img
+      {/*     <img
         src='https://cdn.atomix.vg/wp-content/uploads/2021/06/New-Project-31.jpg'
         alt='Girl in a jacket'
-      ></img>
-      <div className='wrapper__text'>
+      ></img> */}
+      <div className="wrapper__text">
         <h1>{title}</h1>
         <p>{text}</p>
       </div>
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,67 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "../../components/post";
 import "./styles.scss";
-function Blog() {
-  async function getData(
-    url = "https://watermelon-server.herokuapp.com/post/all",
-    data = {}
-  ) {
-    // Opciones por defecto estan marcadas con un *
-    const response = await fetch(url, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      mode: "no-cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      // credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      // body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
+import axios from "axios";
 
-  getData();
+const Blog = () => {
+  const [postList, setPostList] = useState([]);
+
+  const URL = "https://watermelon-server.herokuapp.com/post/all";
+
+  const fetchPost = async () => {
+    await axios
+      .get(URL)
+      .then((response) => {
+        setPostList(response.data.post);
+        console.log(response.data.post);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  useEffect(() => {
+    postList.forEach((element) => console.log(element));
+  }, [postList]);
 
   return (
     <>
-      <Post
-        bgColor="red"
-        title="Titulo"
-        imgUrl=""
-        text="In in officia incididunt enim anim labore est nostrud excepteur amet.
-          Aute velit occaecat amet labore ea occaecat duis dolore culpa eiusmod
-          ipsum do culpa. Esse incididunt ullamco velit aliqua consequat veniam
-          duis. Eu eiusmod esse eiusmod nostrud. Eu deserunt est minim aliquip
-          consequat irure et enim excepteur. Sunt cupidatat duis tempor ipsum ut
-          id in consectetur laborum sint consequat sint id officia."
-      />
-      <Post
-        bgColor="red"
-        title="Titulo"
-        imgUrl=""
-        text="In in officia incididunt enim anim labore est nostrud excepteur amet.
-          Aute velit occaecat amet labore ea occaecat duis dolore culpa eiusmod
-          ipsum do culpa. Esse incididunt ullamco velit aliqua consequat veniam
-          duis. Eu eiusmod esse eiusmod nostrud. Eu deserunt est minim aliquip
-          consequat irure et enim excepteur. Sunt cupidatat duis tempor ipsum ut
-          id in consectetur laborum sint consequat sint id officia."
-      />
-      <Post
-        bgColor="red"
-        title="Titulo"
-        imgUrl=""
-        text="In in officia incididunt enim anim labore est nostrud excepteur amet.
-          Aute velit occaecat amet labore ea occaecat duis dolore culpa eiusmod
-          ipsum do culpa. Esse incididunt ullamco velit aliqua consequat veniam
-          duis. Eu eiusmod esse eiusmod nostrud. Eu deserunt est minim aliquip
-          consequat irure et enim excepteur. Sunt cupidatat duis tempor ipsum ut
-          id in consectetur laborum sint consequat sint id officia."
-      />
+      {postList.map((post) => (
+        <Post
+          bgColor="red"
+          title={post.title}
+          imgUrl=""
+          text={post.paragraph}
+        />
+      ))}
     </>
   );
-}
+};
 
 export default Blog;
